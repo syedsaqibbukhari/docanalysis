15756a1d7ad9544e7697ada9a7b5087844e3c079
Konstantin Baierer <unixprog@gmail.com>
2019-07-29 18:18:59 +0200

    dewarp: invert pix2pixHD path logic

--- ocrd_anybaseocr/cli/ocrd_anybaseocr_dewarp.py
+++ ocrd_anybaseocr/cli/ocrd_anybaseocr_dewarp.py
@@ -33,53 +33,55 @@ class OcrdAnybaseocrDewarper(Processor):
         return cropped
 
     def process(self):
+
         if not torch.cuda.is_available():
             print("Your system has no CUDA installed. No GPU detected.")
             sys.exit(1)
+
         path = self.check_pix2pix()
-        if not (path is None):
-            for (_, input_file) in enumerate(self.input_files):
-                local_input_file = self.workspace.download_file(input_file)
-                pcgts = parse(local_input_file.url, silence=True)
-                image_coords = pcgts.get_Page().get_Border().get_Coords().points.split()
-                fname = pcgts.get_Page().imageFilename
-
-                # Get page Co-ordinates
-                min_x, min_y = image_coords[0].split(",")
-                max_x, max_y = image_coords[2].split(",")
-                img_tmp_dir = "OCR-D-IMG/test_A"
-                img_dir = os.path.dirname(str(fname))
-                # Path of pix2pixHD
-                Path(img_tmp_dir).mkdir(parents=True, exist_ok=True)
-
-                crop_region = int(min_x), int(
-                    min_y), int(max_x), int(max_y)
-                cropped_img = self.crop_image(fname, crop_region)
-
-                base, _ = ocrolib.allsplitext(fname)
-                filename = base.split("/")[-1] + ".png"
-                cropped_img.save(img_tmp_dir + "/" + filename)
-                #os.system("cp %s %s" % (str(fname), os.path.join(img_tmp_dir, os.path.basename(str(fname)))))
-                #os.system("mkdir -p %s" % img_tmp_dir)
-                #os.system("cp %s %s" % (str(fname), os.path.join(img_tmp_dir, os.path.basename(str(fname)))))
-            os.system("python " + str(path) + "/test.py --dataroot %s --checkpoints_dir ./ --name models --results_dir %s --label_nc 0 --no_instance --no_flip --resize_or_crop none --n_blocks_global 10 --n_local_enhancers 2 --gpu_ids %s --loadSize %d --fineSize %d --resize_or_crop %s" %
-                      (os.path.dirname(img_tmp_dir), img_dir, self.parameter['gpu_id'], self.parameter['resizeHeight'], self.parameter['resizeWidth'], self.parameter['imgresize']))
-            synthesized_image = filename.split(
-                ".")[0] + "_synthesized_image.jpg"
-            pix2pix_img_dir = img_dir + "/models/test_latest/images/"
-            dewarped_image = Path(pix2pix_img_dir + synthesized_image)
-            if(dewarped_image.is_file()):
-                shutil.copy(dewarped_image, img_dir + "/" +
-                            filename.split(".")[0] + "_dw.jpg")
-
-            if(Path(img_tmp_dir).is_dir()):
-                shutil.rmtree(img_tmp_dir)
-            if(Path(img_dir + "/models").is_dir()):
-                shutil.rmtree(img_dir + "/models")
-
-        else:
+        if not path:
             print(""" Please check if pix2pixHD is downloaded in path docanalysis/ocrd_anybaseocr
                     If not, you can clone the repository from the following url:
                         https://github.com/NVIDIA/pix2pixHD
                     Alternatively, you can change the pix2pixHD path in ocrd-json file
                     """)
+            sys.exit(1)
+
+        for (_, input_file) in enumerate(self.input_files):
+            local_input_file = self.workspace.download_file(input_file)
+            pcgts = parse(local_input_file.url, silence=True)
+            image_coords = pcgts.get_Page().get_Border().get_Coords().points.split()
+            fname = pcgts.get_Page().imageFilename
+
+            # Get page Co-ordinates
+            min_x, min_y = image_coords[0].split(",")
+            max_x, max_y = image_coords[2].split(",")
+            img_tmp_dir = "OCR-D-IMG/test_A"
+            img_dir = os.path.dirname(str(fname))
+            # Path of pix2pixHD
+            Path(img_tmp_dir).mkdir(parents=True, exist_ok=True)
+
+            crop_region = int(min_x), int(
+                min_y), int(max_x), int(max_y)
+            cropped_img = self.crop_image(fname, crop_region)
+
+            base, _ = ocrolib.allsplitext(fname)
+            filename = base.split("/")[-1] + ".png"
+            cropped_img.save(img_tmp_dir + "/" + filename)
+            #os.system("cp %s %s" % (str(fname), os.path.join(img_tmp_dir, os.path.basename(str(fname)))))
+            #os.system("mkdir -p %s" % img_tmp_dir)
+            #os.system("cp %s %s" % (str(fname), os.path.join(img_tmp_dir, os.path.basename(str(fname)))))
+        os.system("python " + str(path) + "/test.py --dataroot %s --checkpoints_dir ./ --name models --results_dir %s --label_nc 0 --no_instance --no_flip --resize_or_crop none --n_blocks_global 10 --n_local_enhancers 2 --gpu_ids %s --loadSize %d --fineSize %d --resize_or_crop %s" %
+                  (os.path.dirname(img_tmp_dir), img_dir, self.parameter['gpu_id'], self.parameter['resizeHeight'], self.parameter['resizeWidth'], self.parameter['imgresize']))
+        synthesized_image = filename.split(
+            ".")[0] + "_synthesized_image.jpg"
+        pix2pix_img_dir = img_dir + "/models/test_latest/images/"
+        dewarped_image = Path(pix2pix_img_dir + synthesized_image)
+        if(dewarped_image.is_file()):
+            shutil.copy(dewarped_image, img_dir + "/" +
+                        filename.split(".")[0] + "_dw.jpg")
+
+        if(Path(img_tmp_dir).is_dir()):
+            shutil.rmtree(img_tmp_dir)
+        if(Path(img_dir + "/models").is_dir()):
+            shutil.rmtree(img_dir + "/models")
